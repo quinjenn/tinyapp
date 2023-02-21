f45cde0b09a99b49e39ef439b6b6bd3583290a1e
quinjenn <jquintal_@hotmail.com>
2023-02-20 17:38:17 -0800

    added GET and POST routes

--- express_server.js
+++ express_server.js
@@ -3,6 +3,21 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs"); // tells the Express app to use EJS as its templating engine
+app.use(express.urlencoded({ extended: true })); // encode the data to make it readable // Express library's body parsing middleware
+
+// generate a unique short url id
+function generateRandomString() {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < 6) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -33,6 +48,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// GET route, with the path /urls/new
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 // new route handler for "/urls/:id"
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
@@ -40,4 +60,10 @@ app.get("/urls/:id", (req, res) => {
     longURL: "http://www.lighthouselabs.ca",
   };
   res.render("urls_show", templateVars);
+});
+
+// POST request to receive form submission
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
