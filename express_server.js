f084422a5f130c9b43769cb5c8907fcb9e00c6cb
quinjenn <jquintal_@hotmail.com>
2023-02-20 17:03:54 -0800

    updated with views folder

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs"); // tells the Express app to use EJS as its templating engine
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -23,4 +25,19 @@ app.get("/urls.json", (req, res) => {
 // /hello webpage
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+// new route handler for "/urls"
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+// new route handler for "/urls/:id"
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: "http://www.lighthouselabs.ca",
+  };
+  res.render("urls_show", templateVars);
 });
